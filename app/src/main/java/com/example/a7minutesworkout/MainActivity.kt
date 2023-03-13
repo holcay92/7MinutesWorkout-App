5fd75ac25e4e3cf928cbc0980de2db28df4aa2d2
Halil Olcay <holcay92@gmail.com>
2023-03-14 00:01:41 +0300

    view binding added, action bar removed

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton = findViewById<FrameLayout>(R.id.flStart)
+      /* val flStartButton = findViewById<FrameLayout>(R.id.flStart)
         flStartButton.setOnClickListener {
           Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }*/
+
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
     }
 }
