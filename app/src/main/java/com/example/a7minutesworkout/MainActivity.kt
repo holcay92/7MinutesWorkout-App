04a9f7dbe57f38c6178d43a929ce8866a79aea93
Halil Olcay <holcay92@gmail.com>
2023-03-14 01:13:09 +0300

    timer demo in comments added

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.a7minutesworkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.FrameLayout
 import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var countDownTimer : CountDownTimer? = null
+    private var timerDuration : Long = 60000
+    private var pauseOffset : Long = 0
     private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,46 @@ class MainActivity : AppCompatActivity() {
         flStartButton.setOnClickListener {
           Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }*/
+        /*
+        tvTimer.text = "${(timerDuration/1000).toString()}"
+        btnStart.setOnClickListener {
+            startTimer(pauseOffset)
+        }
+        btnPause.setOnClickListener {
+            pauseTimer()
+        }
+        btnStop.setOnClickListener {
+            resetTimer()
+        }
+
+        private fun startTimer(pauseOffset: Long) {
+            countDownTimer = object : CountDownTimer(timerDuration - pauseOffset, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                pauseOffset = timerDuration - millisUntilFinished
+                    tvTimer.text = "${(millisUntilFinished/1000).toString()}"
+                }
+
+                override fun onFinish() {
+                    Toast.makeText(this@MainActivity, "Timer Finished", Toast.LENGTH_SHORT).show()
+                }
+            }.start()
+        }
+
+        private fun pauseTimer() {
+            if(countDownTimer != null){
+                countDownTimer?.cancel()
+            }
+        }
+        private fun resetTimer() {
+            if(countDownTimer != null){
+                countDownTimer?.cancel()
+                pauseOffset = 0
+                countDownTimer = null
+                tvTimer.text = "${(timerDuration/1000).toString()}"
+             }
+         }
+
+        */
         binding?.flStart?.setOnClickListener {
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
 
