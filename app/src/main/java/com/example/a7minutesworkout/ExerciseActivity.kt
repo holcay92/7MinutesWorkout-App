b3952534915992cca3b7dcb7b12d5452ed8bb002
Halil Olcay <holcay92@gmail.com>
2023-03-14 00:48:30 +0300

    exercise activity's progress bar is added

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -8,13 +8,10 @@ class ExerciseActivity : AppCompatActivity() {
     private var binding:ActivityExerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityExerciseBinding.inflate(layoutInflater)
-
         setContentView(binding?.root)
 
         setSupportActionBar(binding?.toolbarExercise)
-
         if (supportActionBar != null){
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
