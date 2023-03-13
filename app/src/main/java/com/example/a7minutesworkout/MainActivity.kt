90895d5b049831517fe6b0fbd77bad6b68b0eac6
Halil Olcay <holcay92@gmail.com>
2023-03-14 00:27:35 +0300

    exercise activity, toolBar and back button added

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -12,14 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-
       /* val flStartButton = findViewById<FrameLayout>(R.id.flStart)
         flStartButton.setOnClickListener {
           Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }*/
-
         binding?.flStart?.setOnClickListener {
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    // To avoid memory leak we need to set the binding to null in onDestroy
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
