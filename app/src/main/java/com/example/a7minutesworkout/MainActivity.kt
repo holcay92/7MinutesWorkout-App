da7ff571cc6e7240b68b38d36f2a118c730ba85f
Halil Olcay <holcay92@gmail.com>
2023-03-13 23:56:18 +0300

    starting screen ui is implemented, ripple added

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton = findViewById<FrameLayout>(R.id.flStart)
+        flStartButton.setOnClickListener {
+          Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
