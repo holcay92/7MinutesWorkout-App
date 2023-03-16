45ccfb7d61740eea4d844a908cb1447654d2172f
Halil Olcay <holcay92@gmail.com>
2023-03-17 00:59:07 +0300

    bmi activity is added

---
+++ app/src/main/java/com/example/a7minutesworkout/BMIActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a7minutesworkout.databinding.ActivityBmiactivityBinding
+
+class BMIActivity : AppCompatActivity() {
+    private var binding: ActivityBmiactivityBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBmiactivityBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarBmiActivity)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "CALCULATE BMI"
+        }
+        binding?.toolbarBmiActivity?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -67,6 +67,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ExerciseActivity::class.java)
             startActivity(intent)
         }
+        binding?.flBMI?.setOnClickListener {
+           val intent = Intent(this, BMIActivity::class.java)
+              startActivity(intent)
+        }
     }
 
     // To avoid memory leak we need to set the binding to null in onDestroy
