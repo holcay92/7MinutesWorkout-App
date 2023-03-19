ab6aa616d2ff90af0dfaeec7b05c561a046ee645
Halil Olcay <holcay92@gmail.com>
2023-03-19 15:32:11 +0300

    history activity dao is implemented and get dates function implemented

--- app/src/main/java/com/example/a7minutesworkout/view/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/view/HistoryActivity.kt
@@ -2,7 +2,12 @@ package com.example.a7minutesworkout.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import com.example.a7minutesworkout.viewModal.HistoryDao
+import com.example.a7minutesworkout.viewModal.WorkOutApp
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
     private var binding: ActivityHistoryBinding? = null
@@ -17,5 +22,25 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        val dao = (application as WorkOutApp).db.historyDao()
+        getAllCompletedDates(dao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao) {
+
+        lifecycleScope.launch {
+            historyDao.fetchAllDates().collect { allCompletedDatesList->
+                // List items are printed in log.
+                for (i in allCompletedDatesList) {
+                    Log.e("Date : ", "" + i)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+// reset the binding to null to avoid memory leak
+        binding = null
     }
 }
--- app/src/main/java/com/example/a7minutesworkout/viewModal/HistoryDao.kt
+++ app/src/main/java/com/example/a7minutesworkout/viewModal/HistoryDao.kt
@@ -2,9 +2,16 @@ package com.example.a7minutesworkout.viewModal
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.a7minutesworkout.modal.HistoryEntity
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface HistoryDao {
     @Insert
     suspend fun insert(historyEntity: HistoryEntity)
+
+    @Query("SELECT *FROM 'history_table'")
+    fun fetchAllDates(): Flow<List<HistoryEntity>>
+    // we don't use suspend here because we are using flow. it takes care of the background thread
 }
